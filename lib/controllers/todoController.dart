1cdde12c952064232cd1406520d440ba3320d957
surapong <surapong@mfu.ac.th>
2021-05-06 11:43:11 +0700

    Load saved tasks

--- lib/controllers/todoController.dart
+++ lib/controllers/todoController.dart
@@ -4,6 +4,8 @@ import 'package:get_storage/get_storage.dart';
 import 'package:state/models/todo.dart';
 import 'package:get/get.dart';
 
+import '../models/todo.dart';
+
 class TodoController extends GetxController {
   // Array of Todo
   // List<Todo> todos;
@@ -17,12 +19,59 @@ class TodoController extends GetxController {
     print('Controller is created');
 
     // =========== Load data ==========
-    
+    // 1. Load data from local storage
+    // List<Map<String,dynamic>> todosSave = GetStorage().read('todos');
+    List todosSave = GetStorage().read('todos');
+    if (todosSave != null) {
+      // print('Data loaded');
+      // print(todosSave);
+      // Our loaded data is a List of Map
+      // [{title: task1, status: false}, {title: task2, status: false}]
+
+      // --------------- Method 1 ---------------
+      /*
+      // 2. Convert List of Map to List of Todo object
+      List<Todo> todosTemp = [];
+      for (int i = 0; i < todosSave.length; i++) {
+        // Map<String, dynamic> task = todosSave[i];
+        // todosTemp.add(Todo(title: task['title'], status: task['status']));
+        todosTemp.add(
+            Todo(title: todosSave[i]['title'], status: todosSave[i]['status']));
+      }
+      // print(todosTemp);
+      // 3. Convert List of Todo object to obs
+      todos = todosTemp.obs;
+      */
+
+      // --------------- Method 2 ---------------
+      // Iterable x = todosSave.map((task) {
+      //   // convert Map to Todo object
+      //   return Todo(title: task['title'], status: task['status']);
+      // });
+      // Iterable x = todosSave.map((task) => Todo(title: task['title'], status: task['status']));
+      // List<Todo> todosTemp = x.toList();
+      // List<Todo> todosTemp = todosSave.map((task) => Todo(title: task['title'], status: task['status'])).toList();
+      // todos = todosTemp.obs;
+
+      // todos = todosSave
+      //     .map((task) => Todo(title: task['title'], status: task['status']))
+      //     .toList()
+      //     .obs;
+
+      // --------------- Method 3 ---------------
+      // todos = todosSave
+      //     .map((task) => Todo().fromJson(task))
+      //     .toList()
+      //     .obs;
+
+      todos = todosSave.map((task) => Todo.fromJson(task)).toList().obs;
+    }
+
     // =========== Create worker to listen to todos changes ==========
     ever(todos, (_) {
       // =========== Save data ==========
       // print('Data changed');
-       
+
       // --------------- Method 1 ---------------
       /*
       // 1. convert obs to simple list
--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -16,7 +16,7 @@ class Todo {
   Todo({this.title, this.status = false});
   // Todo(title: 'hello', status: true);
   // Todo(title: 'hello');
-   
+
   //convert class object to json
   //for Method 1 only
   String toJSON() {
@@ -25,7 +25,17 @@ class Todo {
 
   // flutter method to conver class to Map
   // this method will be called automatically when we use jsonEncode()
+  // toJson is to convert Map or object to JSON
   Map<String, dynamic> toJson() {
     return {'title': title, 'status': status};
+  }
+
+  // fromJson is to convert JSON to object
+  // Todo fromJson(Map<String, dynamic> json) {
+  //   return Todo(title: json['title'], status: json['status']);
+  // }
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Todo(title: json['title'], status: json['status']);
   }
 }
